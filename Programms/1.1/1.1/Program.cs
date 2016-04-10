4bdaa7d748a477eb68ae6b645aa8ba0f26981898
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 11:24:45 +0300

    Programms has been added

---
+++ Programms/1.1/1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности");
+            int n=Int32.Parse(Console.ReadLine());
+            #region
+            for (int i=0; i < n; i++)
+            {
+                Console.Write( ((i%2)==1)?"-1 ":"1 " );
+            }
+            #endregion
+        }
+    }
+}
