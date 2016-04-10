7971349f08dac9aec7dd25ea232d306b61c382c7
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 22:13:25 +0300

    Project 1.7 has been added

---
+++ Programms/1.7/1.7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._7
+{
+    /// <summary>
+    /// Это программа проверяет введенное чило на простоту
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            try
+            {
+                #region
+                int n = Int32.Parse(Console.ReadLine());
+                bool b = true;
+
+                #region
+                for (int i=2; i<= Math.Sqrt(n);i++)
+                {
+                    if (n % i == 0)
+                        b = false;
+                        
+                }
+                #endregion
+
+                if (b==false)
+                    Console.WriteLine("Число " + n + " не является простым");
+                else
+                    Console.WriteLine("Число " + n + " является простым");
+
+                #endregion
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
