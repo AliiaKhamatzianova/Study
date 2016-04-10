8217c6ae1656cdf86d7faf2591ebda5ece2baf6b
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 21:57:47 +0300

    Project 1.6 has been added

---
+++ Programms/1.6/1.6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._6
+{
+    /// <summary>
+    /// Это программа вычисляет НОК двух целых чисел
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            try
+            {
+                #region
+                int a = Int32.Parse(Console.ReadLine());
+                int a1 = a;
+                Console.WriteLine("Введите второе число");
+                int b = Int32.Parse(Console.ReadLine());
+                int b1 = b;
+
+                #region
+                while (b!=0)
+                {
+                    int tmp = a % b;
+                    a = b;
+                    b = tmp;
+                }
+                #endregion
+
+                Console.WriteLine(string.Format("НОК({0},{1})",a1,b1) + " = " + a1*b1/a);
+                #endregion
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
