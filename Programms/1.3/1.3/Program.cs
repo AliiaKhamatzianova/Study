740b2353f3fea5f88a4a627344ce43a8aa6e6e57
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 20:20:28 +0300

    Project 1.3 has been added

---
+++ Programms/1.3/1.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._3
+{
+    /// <summary>
+    /// Это программа для подсчета суммы цифр введенного числа
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число, сумму цифр которого надо посчитать");
+            int n = Int32.Parse(Console.ReadLine());
+
+            int s = 0;
+
+            #region
+            while(n%10!=0)
+            {
+                s += n % 10;
+                n /= 10;
+            }
+            #endregion
+
+            Console.WriteLine("Сумма цифр введенного числа = " + s);
+        }
+    }
+}
