d5d4044732f9f89a4436e5d04d4e048a8812a457
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 20:09:11 +0300

    Project 1.2 has been added

---
+++ Programms/1.2/1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n =Int32.Parse(Console.ReadLine());
+
+            #region
+            while (n%10!=0)
+            {
+                Console.Write(n%10+" ");
+                n /= 10;
+            }
+            #endregion
+        }
+    }
+}
