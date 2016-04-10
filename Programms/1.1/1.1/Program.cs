dbc0239dde7903526bb83707bd6971873ff9b8b9
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 21:03:49 +0300

    In project 1.1 has been added summary and try/catch

--- Programms/1.1/1.1/Program.cs
+++ Programms/1.1/1.1/Program.cs
@@ -8,16 +8,27 @@ namespace _1._1
 {
     class Program
     {
+        /// <summary>
+        /// Это программа для вывода последовательности из 1/-1 заданной пользователем длины 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Введите длину последовательности");
-            int n=Int32.Parse(Console.ReadLine());
-            #region
-            for (int i=0; i < n; i++)
+            try
             {
-                Console.Write( ((i%2)==1)?"-1 ":"1 " );
+                int n = Int32.Parse(Console.ReadLine());
+                #region
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write(((i % 2) == 1) ? "-1 " : "1 ");
+                }
+                #endregion
             }
-            #endregion
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
