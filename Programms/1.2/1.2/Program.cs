6736432332b6e83d01d96e67e331abc92536d712
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-10 21:07:43 +0300

    In project 1.2 has been added summary and catch/try

--- Programms/1.2/1.2/Program.cs
+++ Programms/1.2/1.2/Program.cs
@@ -6,20 +6,30 @@ using System.Threading.Tasks;
 
 namespace _1._2
 {
+    /// <summary>
+    /// Это программа выводит цифры введенного числа в обратном порядке
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число");
-            int n =Int32.Parse(Console.ReadLine());
-
-            #region
-            while (n%10!=0)
+            try
             {
-                Console.Write(n%10+" ");
-                n /= 10;
+                int n = Int32.Parse(Console.ReadLine());
+
+                #region
+                while (n % 10 != 0)
+                {
+                    Console.Write(n % 10 + " ");
+                    n /= 10;
+                }
+                #endregion
             }
-            #endregion
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
