2701a421fe29caa1043aff789c28ef320ba40287
Aliia Khamatzianova <aliiakhamatzianova@outlook.com>
2016-04-11 22:13:48 +0300

    Добавлен проект 1.10

---
+++ Programms/1.10/1.10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._10
+{
+    /// <summary>
+    /// Это программа выводит все делители натурального числа
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            try
+            {
+                int n = Int32.Parse(Console.ReadLine());
+
+                Console.Write("Делители числа " + n + " : ");
+                for (int i=1; i<=n/2;i++)
+                {
+                    if (n % i == 0)
+                        Console.Write(i+" ");
+                }
+                        Console.Write(n+" ");
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
